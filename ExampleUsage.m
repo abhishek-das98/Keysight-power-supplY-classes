a04ddb42791c51e1ca6acca245ce87a82b791940
Abhishek Das <82112245+abhishek-das98@users.noreply.github.com>
2024-11-19 23:28:25 -0500

    Update ExampleUsage.m

--- ExampleUsage.m
+++ ExampleUsage.m
@@ -1,1 +1,12 @@
 
+obj = ClassKeySightSupply.getInstance() % Creates an instance of the class Keysight supply
+obj.connect('SinglePowerSupply') % Select one from two power supplies: SinglePowerSupply with one output and TriplePowerSupply with three outputs
+
+obj.ReadVoltage([]) % Command to Read the voltage
+obj.ReadCurrent([])  % Command to Read the current
+
+obj.powerOn([])  % Command to activate the port
+
+obj. setVoltage([5]) % To set the voltage to 5V
+obj.powerOff([])  % Command to deactivate the port
+
